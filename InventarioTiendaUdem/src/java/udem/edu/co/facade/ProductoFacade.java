82013b9e7e74e0d4b8b7916e6c36999a9d32b445
jordana98 <jordanaapolinariosimon@gmail.com>
2019-03-26 22:41:09 -0500

    Agregado-loggin

---
+++ InventarioTiendaUdem/src/java/udem/edu/co/facade/ProductoFacade.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package udem.edu.co.facade;
+
+import javax.ejb.Stateless;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import udem.edu.co.entity.Producto;
+
+/**
+ *
+ * @author Jordana Apolinario
+ */
+@Stateless
+public class ProductoFacade extends AbstractFacade<Producto> {
+    @PersistenceContext(unitName = "tiendaudem__pm")
+    private EntityManager em;
+
+    @Override
+    protected EntityManager getEntityManager() {
+        return em;
+    }
+
+    public ProductoFacade() {
+        super(Producto.class);
+    }
+    
+}
